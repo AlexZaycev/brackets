b53db623e459d26d6c32966835ae05c22337e114
Alex Zaycev <lexx.zaycev@gmail.com>
2022-08-12 22:34:43 +0300

    complete brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = '';
+  const bracketsPair = bracketsConfig.map((item) => item[0] + item[1]);
+
+  for (let bracket of str) {
+    brackets += bracket;
+
+    bracketsPair.forEach((pair) => {
+      if (brackets.endsWith(pair)) {
+        brackets = brackets.slice(0, brackets.length - 2);
+      }
+    });
+  }
+
+  return brackets.length === 0;
 }
